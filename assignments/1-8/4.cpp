b7f776b47bf36af7622026e98b1955b13edc1092
iTzVoko <vokoads@gmail.com>
2023-07-05 19:56:12 +0300

    Add `4.cpp` solution (1-8)

---
+++ assignments/1-8/4.cpp
@@ -0,0 +1,17 @@
+/*
+ * بسم الله الرحمن الرحيم
+ *
+ * Solved by: @iTzVoko
+ */
+#include <iostream>
+
+int main()
+{
+	std::cout
+		<< "|=======================|\n"
+		<< "|== Elzero Web School ==|\n"
+		<< "|==        C++        ==|\n"
+		<< "|=======================|\n";
+
+	return 0;
+}
