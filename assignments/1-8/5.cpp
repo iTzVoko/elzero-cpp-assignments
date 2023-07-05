fb7195b19abf7851b0149e7c081c1d8a4299dd4c
iTzVoko <vokoads@gmail.com>
2023-07-05 21:43:16 +0300

    Update: change block comment to inline comment in `5.cpp`

--- assignments/1-8/5.cpp
+++ assignments/1-8/5.cpp
@@ -2,7 +2,7 @@
  * بسم الله الرحمن الرحيم
  *
  * Solved by: @iTzVoko
- * Note: I did 5 fixes to the comments
+ * Note: I did 6 fixes to the comments
  */
 // This Is My First File
 // In Learning Programming
@@ -12,7 +12,7 @@
 int main()
 {
 	std::cout /* Character Output */ << "Line One\n";
-	std::cout << "Line Two\n"; /* cout Function */
+	std::cout << "Line Two\n"; // cout Function
 	std::cout << "Line Three\n";
 
 	return 0;
