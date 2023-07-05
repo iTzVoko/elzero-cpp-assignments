5f1cc36b9e0e8427c00d2cf11515361be3d194f8
iTzVoko <vokoads@gmail.com>
2023-07-05 19:23:06 +0300

    Add `3.cpp` solution (1-8)

---
+++ assignments/1-8/3.cpp
@@ -0,0 +1,19 @@
+/*
+ * بسم الله الرحمن الرحيم
+ *
+ * Solved by: @iTzVoko
+ */
+#include <iostream>
+
+int main()
+{
+	std::cout
+		<< "Iam Learning Programming\n"
+		<< "With\n"
+		<< "Elzero\n"
+		<< "Web\n"
+		<< "School\n"
+		<< "<3\n";
+
+	return 0;
+}
