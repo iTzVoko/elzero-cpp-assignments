7d1b5bc634e4a490a280f7b32227e2b3f7aba228
iTzVoko <vokoads@gmail.com>
2023-07-05 16:25:15 +0300

    Add 2.cpp solution (1-8)

---
+++ assignments/1-8/2.cpp
@@ -0,0 +1,17 @@
+/*
+ * بسم الله الرحمن الرحيم
+ *
+ * Solved by: @iTzVoko
+ * Note: https://cplusplus.com/reference/iostream/
+ */
+/*
+  iostream Header File Content
+  -- cout
+  -- cin
+  -- cerr
+  -- clog
+  -- wcout
+  -- wcin
+  -- wcerr
+  -- wclog
+*/
