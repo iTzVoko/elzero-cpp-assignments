2153c07b9cb47dedcbaff9faa7466a48743e2a52
iTzVoko <vokoads@gmail.com>
2023-07-05 19:57:51 +0300

    Add `5.cpp` solution (1-8) (finish)

---
+++ assignments/1-8/5.cpp
@@ -0,0 +1,19 @@
+/*
+ * بسم الله الرحمن الرحيم
+ *
+ * Solved by: @iTzVoko
+ * Note: I did 5 fixes to the comments
+ */
+// This Is My First File
+// In Learning Programming
+
+#include <iostream> /* Preprocessor Directive */
+
+int main()
+{
+	std::cout /* Character Output */ << "Line One\n";
+	std::cout << "Line Two\n"; /* cout Function */
+	std::cout << "Line Three\n";
+
+	return 0;
+} // End Main Function
