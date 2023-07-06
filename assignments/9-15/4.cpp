7cb5a85fb8bd0806562eacde392dae8f73ed4800
iTzVoko <vokoads@gmail.com>
2023-07-06 22:26:55 +0300

    Add `4.cpp` solution (9-15)

---
+++ assignments/9-15/4.cpp
@@ -0,0 +1,17 @@
+/*
+ * بسم الله الرحمن الرحيم
+ *
+ * Solved by: @iTzVoko
+ */
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	// Write my_text Variable Content Here
+	string my_text = "============\n== Elzero ==\n============";
+
+	// Do Not Edit Below
+	cout << my_text;
+	return 0;
+}
