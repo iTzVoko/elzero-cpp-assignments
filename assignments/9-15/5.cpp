45976064a2dd6c3a73f711d0c6b901d4e62865b9
iTzVoko <vokoads@gmail.com>
2023-07-07 00:15:24 +0300

    Add `5.cpp` solution (9-15)

---
+++ assignments/9-15/5.cpp
@@ -0,0 +1,20 @@
+/*
+ * بسم الله الرحمن الرحيم
+ *
+ * Solved by: @iTzVoko
+ */
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	// Do Not Edit Below
+	int a, b, c = 100;
+
+	// Add Your Code Here
+	a = b = c;
+
+	// Do Not Edit Below
+	cout << a + b + c; // 300
+	return 0;
+}
