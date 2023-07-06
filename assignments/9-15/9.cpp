54e683dd517c8dcd8b817198d820fcf4d206751c
iTzVoko <vokoads@gmail.com>
2023-07-07 00:16:31 +0300

    Add `9.cpp` solution (9-15)

---
+++ assignments/9-15/9.cpp
@@ -0,0 +1,23 @@
+/*
+ * بسم الله الرحمن الرحيم
+ *
+ * Solved by: @iTzVoko
+ */
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	// Write Your Code Here
+	int num;
+	cout << "Enter a number: ";
+	cin >> num;
+
+	int result = num * num;
+
+	cout << "Result: " << result << '\n';
+
+	// Num 10
+	// Needed Output 100 [10 * 10]
+	return 0;
+}
