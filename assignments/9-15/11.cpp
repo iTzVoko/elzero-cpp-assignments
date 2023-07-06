378d2e11c9e057eab3499eeca14afe7d30d21318
iTzVoko <vokoads@gmail.com>
2023-07-07 00:17:25 +0300

    Add `11.cpp` solution (9-15) (finish)

---
+++ assignments/9-15/11.cpp
@@ -0,0 +1,20 @@
+/*
+ * بسم الله الرحمن الرحيم
+ *
+ * Solved by: @iTzVoko
+ */
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << "Bello\r\"Hello Elzeroo\b";
+	cout << " Web Schoolll\b\b";
+	cout << " I Love Programming";
+	cout << " Too Much";
+	cout << " Specially C++\"\n";
+	return 0;
+}
+
+// Needed Output
+// "Hello Elzero Web School I Love Programming Too Much Specially C++"
