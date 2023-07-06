0cdbe7eeb1f7d9de59bf0d0102faf3b8aae62707
iTzVoko <vokoads@gmail.com>
2023-07-07 00:15:54 +0300

    Add `7.cpp` solution (9-15)

---
+++ assignments/9-15/7.cpp
@@ -0,0 +1,29 @@
+/*
+ * بسم الله الرحمن الرحيم
+ *
+ * Solved by: @iTzVoko
+ */
+#include <iostream>
+using namespace std;
+
+int num = 100;
+
+int read()
+{
+	int num = 100;
+	cout << num;
+	return 0;
+}
+
+int play()
+{
+	int num = 200;
+	read();
+	return 0;
+}
+
+int main()
+{
+	play();
+	return 0;
+}
