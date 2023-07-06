00b1bd158d90c602b2c81e6c9f4e5d8e75d127ea
iTzVoko <vokoads@gmail.com>
2023-07-07 00:15:43 +0300

    Add `6.cpp` solution (9-15)

---
+++ assignments/9-15/6.cpp
@@ -0,0 +1,18 @@
+/*
+ * بسم الله الرحمن الرحيم
+ *
+ * Solved by: @iTzVoko
+ */
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	// Do Not Edit Below
+	int a = 210, b = -50, c = -40;
+
+	// Write Your cout Code Here
+	cout << a - b - c << '\n';
+
+	return 0;
+}
