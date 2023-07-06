30217e1d5a6be23b074a7489ced0de10f8c367e4
iTzVoko <vokoads@gmail.com>
2023-07-07 00:16:52 +0300

    Add `10.cpp` solution (9-15)

---
+++ assignments/9-15/10.cpp
@@ -0,0 +1,28 @@
+/*
+ * بسم الله الرحمن الرحيم
+ *
+ * Solved by: @iTzVoko
+ */
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	// Write Your Code Here
+	int numOne, numTwo;
+
+	cout << "Enter the first number: ";
+	cin >> numOne;
+
+	cout << "Enter the second number: ";
+	cin >> numTwo;
+
+	int result = numOne * numTwo;
+
+	cout << "Result: " << result << '\n';
+
+	// Number One => 10
+	// Number Two => 100
+	// Needed Output 1000 [10 * 100]
+	return 0;
+}
