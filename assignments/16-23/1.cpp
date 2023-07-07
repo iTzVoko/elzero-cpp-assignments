3391baccc2c782d83bd3ef208fcd4074f3638061
iTzVoko <vokoads@gmail.com>
2023-07-07 09:48:17 +0300

    Add `1.cpp` solution (16-23)

---
+++ assignments/16-23/1.cpp
@@ -0,0 +1,17 @@
+/*
+ * بسم الله الرحمن الرحيم
+ *
+ * Solved by: @iTzVoko
+ */
+// Example
+false	// bool => Boolean
+
+'a'		// char => Character
+'9' 	// char => Character
+9		// int => Integer
+true	// bool => Boolean
+100		// int => Integer
+-10 	// int => Integer
+0		// int => Integer
+false	// bool => Boolean
+10.9	// double => Double
