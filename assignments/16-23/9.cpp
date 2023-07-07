748bc73f9b950cb9c95a873260c9961f26f6ca92
iTzVoko <vokoads@gmail.com>
2023-07-07 23:52:09 +0300

    Add `9.cpp` solution (16-23)

---
+++ assignments/16-23/9.cpp
@@ -0,0 +1,25 @@
+/*
+ * بسم الله الرحمن الرحيم
+ *
+ * Solved by: @iTzVoko
+ */
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	// Edit Anything Except Values
+	unsigned int a = 100;
+	int b = -100;
+	short c = 100;
+	double d = 500.55;
+
+	// Do Not Edit
+	cout << a << "\n"; // 100
+	cout << b << "\n"; // -100
+	cout << c << "\n"; // 100
+	cout << d << "\n"; // 500.55
+
+	return 0;
+}
