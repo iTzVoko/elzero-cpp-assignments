079e01d2a2bdbe482e92d11629177338a22f0f2f
iTzVoko <vokoads@gmail.com>
2023-07-07 12:23:56 +0300

    Add `5.cpp` solution (16-23)

---
+++ assignments/16-23/5.cpp
@@ -0,0 +1,29 @@
+/*
+ * بسم الله الرحمن الرحيم
+ *
+ * Solved by: @iTzVoko
+ */
+#include <iostream>
+
+int main()
+{
+	char a = '~';
+	char b = '&';
+	char c = '%';
+	char d = 'A';
+
+	// Output Needed
+	// "ASCII Value of ~ Is 126"
+	std::cout << "ASCII Value of " << a << " Is " << int(a) << '\n';
+
+	// "ASCII Value of & Is 38"
+	std::cout << "ASCII Value of " << b << " Is " << int(b) << '\n';
+
+	// "ASCII Value of % Is 37"
+	std::cout << "ASCII Value of " << c << " Is " << int(c) << '\n';
+
+	// "ASCII Value of A Is 65"
+	std::cout << "ASCII Value of " << d << " Is " << int(d) << '\n';
+
+	return 0;
+}
