9d3b586171cf2c13c33282f5097535798b01ba74
iTzVoko <vokoads@gmail.com>
2023-07-07 12:14:10 +0300

    Add `4.cpp` solution (16-23)

---
+++ assignments/16-23/4.cpp
@@ -0,0 +1,16 @@
+/*
+ * بسم الله الرحمن الرحيم
+ *
+ * Solved by: @iTzVoko
+ */
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	// Edit Line To Make 8 Become 4
+	cout << sizeof(10.5f + 5 + 20.5f + 10) << "\n"; // 8
+
+	return 0;
+}
