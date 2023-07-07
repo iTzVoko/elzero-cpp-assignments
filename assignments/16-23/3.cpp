0738154c2f80da2c7deb1a16b259bd67950df6c2
iTzVoko <vokoads@gmail.com>
2023-07-07 11:20:43 +0300

    Add `3.cpp` solution (16-23)

---
+++ assignments/16-23/3.cpp
@@ -0,0 +1,26 @@
+/*
+ * بسم الله الرحمن الرحيم
+ *
+ * Solved by: @iTzVoko
+ */
+#include <iostream>
+#include <limits.h>
+
+int main()
+{
+	std::cout << "Maximum Integer Number Is => " << INT_MAX << '\n';
+	std::cout << "Minimum Integer Number Is => " << INT_MIN << '\n';
+
+	std::cout << "Maximum Short Integer Number Is => " << SHRT_MAX << '\n';
+	std::cout << "Minimum Short Integer Number Is => " << SHRT_MIN << '\n';
+
+	return 0;
+}
+
+// Output Needed
+/*
+	"Maximum Integer Number Is => 2147483647"
+	"Minimum Integer Number Is => -2147483648"
+	"Maximum Short Integer Number Is => 32767"
+	"Minimum Short Integer Number Is => -32768"
+ */
