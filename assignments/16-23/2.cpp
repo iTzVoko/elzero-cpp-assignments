9600585e5d878cd6c173e456d52d9006e5411365
iTzVoko <vokoads@gmail.com>
2023-07-07 11:14:24 +0300

    Add `2.cpp` solution (16-23)

---
+++ assignments/16-23/2.cpp
@@ -0,0 +1,18 @@
+/*
+ * بسم الله الرحمن الرحيم
+ *
+ * Solved by: @iTzVoko
+ */
+#include <iostream>
+
+int main()
+{
+
+	double salary = 5000.98;
+
+	// 8 Bytes
+	std::cout << sizeof(salary) << '\n';
+
+	// 64 Bits
+	std::cout << sizeof(salary) * 8 << '\n';
+}
