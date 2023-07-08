63a1cc36e45d31f84d0c5a46f637fc3c34d38814
iTzVoko <vokoads@gmail.com>
2023-07-08 20:56:49 +0300

    Add `11.cpp` solution (16-23)

---
+++ assignments/16-23/11.cpp
@@ -0,0 +1,21 @@
+/*
+ * بسم الله الرحمن الرحيم
+ *
+ * Solved by: @iTzVoko
+ */
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	// Do Not Edit Here
+	short int a = 100;
+	float b = 98.5f;
+	double c = 1.7;
+
+	// Edit This Line And Use Type Casting To Get The Output
+	cout << a + int(b) + sizeof(short(c)) << "\n"; // 200
+
+	return 0;
+}
