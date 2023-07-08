57cacec149f66a9d8dc740e937328cfe66273d91
iTzVoko <vokoads@gmail.com>
2023-07-08 10:07:45 +0300

    Add `10.cpp` solution (16-23)

---
+++ assignments/16-23/10.cpp
@@ -0,0 +1,22 @@
+/*
+ * بسم الله الرحمن الرحيم
+ *
+ * Solved by: @iTzVoko
+ */
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	// Add Type Alias Here
+	typedef long long int lli;
+	using lli = long long int;
+	#define lli long long int
+
+	// Do Not Edit
+	lli num = 150050005; // This Is Long Long Int
+	cout << num << "\n"; // 150050005
+
+	return 0;
+}
