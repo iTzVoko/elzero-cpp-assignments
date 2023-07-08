ee7ff60c69c55c8455ccb24e7ec789d58834f981
iTzVoko <vokoads@gmail.com>
2023-07-08 20:52:28 +0300

    Add `8.cpp` solution (16-23)

---
+++ assignments/16-23/8.cpp
@@ -0,0 +1,23 @@
+/*
+ * بسم الله الرحمن الرحيم
+ *
+ * Solved by: @iTzVoko
+ */
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	// Edit Anything Except Values
+	int a = 100;
+	double b = 15001500;
+	long double c = 100.54565746;
+
+	// Do Not Edit
+	cout << sizeof(a) << " Bytes\n"; // 2 Bytes
+	cout << sizeof(b) << " Bytes\n"; // 8 Bytes
+	cout << sizeof(c) << " Bytes\n"; // 16 Bytes
+
+	return 0;
+}
