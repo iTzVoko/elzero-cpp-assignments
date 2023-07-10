113f28c5f82abafadf008e8523ce1aa6bdc26e33
iTzVoko <vokoads@gmail.com>
2023-07-10 16:02:25 +0300

    Add `3.cpp` solution (24-29)

---
+++ assignments/24-29/3.cpp
@@ -0,0 +1,25 @@
+/*
+ * بسم الله الرحمن الرحيم
+ *
+ * Solved by: @iTzVoko
+ */
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	// Do Not Edit
+	int num = 3;
+
+	/*
+	  Write Code Here
+	  Do Not Use num = 6 :)
+	*/
+	num += num;
+
+	// Do Not Edit
+	cout << num << "\n"; // 6
+
+	return 0;
+}
