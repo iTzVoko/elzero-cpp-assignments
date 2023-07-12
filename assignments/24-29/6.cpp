9252763cc405ebdd7851b1f4090ce3a25b5bc636
iTzVoko <vokoads@gmail.com>
2023-07-12 19:30:34 +0300

    Update `6.cpp` solution (24-29) remove != operator

--- assignments/24-29/6.cpp
+++ assignments/24-29/6.cpp
@@ -11,28 +11,28 @@ int main()
 {
 	/*
   We Need To Use This Operators In Solutions
-  - &&
-  - >=
-  - <=
-  - >
-  - <
-  - ==
-  - !
-  - ||
+  - &&	DONE
+  - >=	DONE
+  - <=	DONE
+  - >	DONE
+  - <	DONE
+  - ==	DONE
+  - !	DONE
+  - ||	DONE
 */
 
 	int a = 10;
 	int b = 25;
 	int c = 15;
 
-	cout << (a < b) << "\n";			// True
-	cout << (c > a) << "\n";			// True
-	cout << (a + c == b) << "\n";		// True
-	cout << (c >= a) << "\n";			// True
-	cout << (c <= b) << "\n";			// True
-	cout << (b > a && b > c) << "\n";	// True
-	cout << !(a == b) << "\n";			// True
-	cout << (a != b || b != a) << "\n"; // True
+	cout << (a < b) << "\n";		  // True
+	cout << (c > a) << "\n";		  // True
+	cout << (a + c == b) << "\n";	  // True
+	cout << (c >= a) << "\n";		  // True
+	cout << (c <= b) << "\n";		  // True
+	cout << (b > a && b > c) << "\n"; // True
+	cout << !(a == b) << "\n";		  // True
+	cout << (a < b || b > a) << "\n"; // True
 
 	return 0;
 }
