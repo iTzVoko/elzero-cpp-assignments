465de2f461441c054e2312fb33ef7a0d1d9247e0
iTzVoko <vokoads@gmail.com>
2023-07-12 18:57:17 +0300

    Add `5.cpp` solution (24-29)

---
+++ assignments/24-29/5.cpp
@@ -0,0 +1,23 @@
+/*
+ * بسم الله الرحمن الرحيم
+ *
+ * Solved by: @iTzVoko
+ * Note: You can check the ASCII Value (number) of a certain character by using int(the_character)
+ * 		 For example, int('\n') will result the number 10. int('A') will result the number 65, etc.
+ */
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	// Assign The Values
+	char a = '\n'; // \n equals 10
+	char b = 'c';  // c equals 99
+	char c = '\n'; // \n equals 10
+
+	// Do Not Edit
+	cout << b * c + a << "\n"; // 1000
+
+	return 0;
+}
