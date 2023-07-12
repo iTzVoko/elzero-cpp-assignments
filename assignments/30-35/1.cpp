c8d080f32f337533ae4f2d8790445cd1d90d0b4e
iTzVoko <vokoads@gmail.com>
2023-07-13 00:15:35 +0300

    Add `1.cpp` solution (30-35)

---
+++ assignments/30-35/1.cpp
@@ -0,0 +1,23 @@
+/*
+ * بسم الله الرحمن الرحيم
+ *
+ * Solved by: @iTzVoko
+ */
+#include <iostream>
+
+int main()
+{
+	int age = 40;
+	int points = 800;
+	float rate = 8.5f;
+
+	// Your Condition Here
+	if (age > 18 && points > 500 && rate > 5)
+	{
+		// Output Needed If All Conditions Is True
+		// "Yes Age > 18 & Points > 500 & Rate > 5"
+		std::cout << "Yes Age > 18 & Points > 500 & Rate > 5\n";
+	}
+
+	return 0;
+}
