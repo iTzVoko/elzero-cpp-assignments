74cb3d0d074d4c7d34bd99cd10f0dae7d4512532
iTzVoko <vokoads@gmail.com>
2023-07-12 18:43:30 +0300

    Add `4.cpp` solution (24-29)

---
+++ assignments/24-29/4.cpp
@@ -0,0 +1,15 @@
+/*
+ * بسم الله الرحمن الرحيم
+ *
+ * Solved by: @iTzVoko
+ */
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cout << ((int)10.5 * 5) + int(40.5 + 10.1) << "\n"; // 100
+
+	return 0;
+}
