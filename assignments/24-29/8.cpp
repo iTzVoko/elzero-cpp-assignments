29e67cfae955b3a4466fdcc53af549a5cb5974fd
iTzVoko <vokoads@gmail.com>
2023-07-12 21:20:16 +0300

    Add `8.cpp` solution (24-29) (finish)

---
+++ assignments/24-29/8.cpp
@@ -0,0 +1,19 @@
+/*
+ * بسم الله الرحمن الرحيم
+ *
+ * Solved by: @iTzVoko
+ */
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int num = 10;
+
+	cout << --num	<< " | " << num++ << " | " << ++num << "\n"; // 9 | 9 | 11
+	cout << num		<< " | " << ++num << " | " << num	<< "\n"; // 11 | 12 | 12
+	cout << --num	<< " | " << --num << " | " << --num << "\n"; // 11 | 10 | 9
+
+	return 0;
+}
