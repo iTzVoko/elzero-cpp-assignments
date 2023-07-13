25d5fac23f2907c7065e533893a586cbdcb091bb
iTzVoko <vokoads@gmail.com>
2023-07-13 09:28:15 +0300

    Add `6.cpp` solution (30-35)

---
+++ assignments/30-35/6.cpp
@@ -0,0 +1,28 @@
+/*
+ * بسم الله الرحمن الرحيم
+ *
+ * Solved by: @iTzVoko
+ */
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int age = 40;
+	int points = 100;
+
+	if (sizeof(age) == 4)
+		cout << "Age Data Is 4 Bytes\n";
+	if (age > 18)
+		cout << "Age Is Ok\n";
+	if (points > 50)
+		cout << "Points Is Ok\n";
+
+	// Output Needed
+	// "Age Data Is 4 Bytes"
+	// "Age Is Ok"
+	// "Points Is Ok"
+
+	return 0;
+}
