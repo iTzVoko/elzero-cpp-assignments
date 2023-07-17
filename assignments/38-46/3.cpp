0d3e78e95d02caac669cf7f3a2507825d217ba07
iTzVoko <vokoads@gmail.com>
2023-07-17 23:58:38 +0300

    Add `3.cpp` solution (38-46)

---
+++ assignments/38-46/3.cpp
@@ -0,0 +1,24 @@
+/*
+ * بسم الله الرحمن الرحيم
+ *
+ * Solved by: @iTzVoko
+ */
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int filling = 10;
+	int vals[]{100, 200, 300, 400};
+
+	// Write One Line Of Code Here To Fill Array Value With Number 10
+	vals[3] = vals[2] = vals[1] = vals[0] = filling;
+
+	cout << vals[0] << "\n"; // 10
+	cout << vals[1] << "\n"; // 10
+	cout << vals[2] << "\n"; // 10
+	cout << vals[3] << "\n"; // 10
+
+	return 0;
+}
