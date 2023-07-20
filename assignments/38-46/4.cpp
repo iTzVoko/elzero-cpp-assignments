4d7d934d2ee2dcacc082325b1046f71553d7e818
iTzVoko <vokoads@gmail.com>
2023-07-20 12:27:05 +0300

    Fix solutions order (38-46)

--- assignments/38-46/4.cpp
+++ assignments/38-46/4.cpp
@@ -10,58 +10,95 @@ int main()
 {
 	// Example 1
 	{
-		int vals[] = {100, 200, 600, 200, 100};
+		int vals[]{100, 200, 250, 400, 200};
 
-		// Output
-		// "Array Is Palindrome"
-
-		int lastIndex = std::size(vals) - 1;
-
-		if (vals[0] == vals[lastIndex] && vals[1] == vals[lastIndex - 1])
+		// Needed Output
+		// "First Number + Last Number Is Larger Than Middle Number"
+		// "100 + 200 = 300"
+		// "300 > 250"
+		if (vals[0] + vals[4] > vals[2])
 		{
-			std::cout << "Array Is Palindrome\n";
+			std::cout
+				<< "First Number + Last Number Is Larger Than Middle Number\n"
+				<< vals[0] << " + " << vals[4] << '\n'
+				<< vals[0] + vals[4] << " > " << vals[2] << '\n';
+		}
+		else if (vals[1] + vals[3] > vals[2])
+		{
+			std::cout
+				<< "Second Number + Before Last Number Is Larger Than Middle Number\n"
+				<< vals[1] << " + " << vals[3] << '\n'
+				<< vals[1] + vals[3] << " > " << vals[2] << '\n';
 		}
 		else
 		{
-			std::cout << "Array Is Not Palindrome\n";
+			std::cout
+				<< "Middle Number Is Larger Than Middle Number\n"
+				<< vals[1] << " + " << vals[3] << '\n'
+				<< vals[1] + vals[3] << " > " << vals[2] << '\n';
 		}
 	}
 
 	// Example 2
 	{
-		int vals[] = {100, 200, 200, 100};
+		int vals[]{100, 200, 500, 400, 200};
 
-		// Output
-		// "Array Is Palindrome"
+		// Needed Output
+		// "Second Number + Before Last Number Is Larger Than Middle Number"
+		// "200 + 400 = 600"
+		// "600 > 500"
 
-		int lastIndex = std::size(vals) - 1;
-
-		if (vals[0] == vals[lastIndex] && vals[1] == vals[lastIndex - 1])
+		if (vals[0] + vals[4] > vals[2])
 		{
-			std::cout << "Array Is Palindrome\n";
+			std::cout
+				<< "First Number + Last Number Is Larger Than Middle Number\n"
+				<< vals[0] << " + " << vals[4] << '\n'
+				<< vals[0] + vals[4] << " > " << vals[2] << '\n';
+		}
+		else if (vals[1] + vals[3] > vals[2])
+		{
+			std::cout
+				<< "Second Number + Before Last Number Is Larger Than Middle Number\n"
+				<< vals[1] << " + " << vals[3] << '\n'
+				<< vals[1] + vals[3] << " > " << vals[2] << '\n';
 		}
 		else
 		{
-			std::cout << "Array Is Not Palindrome\n";
+			std::cout
+				<< "Middle Number Is Larger Than Middle Number\n"
+				<< vals[1] << " + " << vals[3] << '\n'
+				<< vals[1] + vals[3] << " > " << vals[2] << '\n';
 		}
 	}
 
 	// Example 3
 	{
-		int vals[] = {100, 300, 600, 200, 100};
+		int vals[]{100, 200, 600, 400, 200};
 
-		// Output
-		// "Array Is Not Palindrome"
+		// Needed Output
+		// "Middle Number Is The Largest"
+		// "600"
 
-		int lastIndex = std::size(vals) - 1;
-
-		if (vals[0] == vals[lastIndex] && vals[1] == vals[lastIndex - 1])
+		if (vals[0] + vals[4] > vals[2])
 		{
-			std::cout << "Array Is Palindrome\n";
+			std::cout
+				<< "First Number + Last Number Is Larger Than Middle Number\n"
+				<< vals[0] << " + " << vals[4] << '\n'
+				<< vals[0] + vals[4] << " > " << vals[2] << '\n';
+		}
+		else if (vals[1] + vals[3] > vals[2])
+		{
+			std::cout
+				<< "Second Number + Before Last Number Is Larger Than Middle Number\n"
+				<< vals[1] << " + " << vals[3] << '\n'
+				<< vals[1] + vals[3] << " > " << vals[2] << '\n';
 		}
 		else
 		{
-			std::cout << "Array Is Not Palindrome\n";
+			std::cout
+				<< "Middle Number Is Larger Than Middle Number\n"
+				<< vals[1] << " + " << vals[3] << '\n'
+				<< vals[1] + vals[3] << " > " << vals[2] << '\n';
 		}
 	}
 
