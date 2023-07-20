47e2014b0be0f37885b622bd777f14884d0b8e17
iTzVoko <vokoads@gmail.com>
2023-07-20 14:58:55 +0300

    Add `8.cpp` solution (38-46)

---
+++ assignments/38-46/8.cpp
@@ -0,0 +1,22 @@
+/*
+ * بسم الله الرحمن الرحيم
+ *
+ * Solved by: @iTzVoko
+ */
+#include <array>
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  // Your Code Here
+  array<int, 3> nums = {0};
+
+  // Do Not Edit
+  nums.fill(100);
+  cout << nums[0] << "\n"; // 100
+  cout << nums[1] << "\n"; // 100
+  cout << nums[2] << "\n"; // 100
+
+  return 0;
+}
