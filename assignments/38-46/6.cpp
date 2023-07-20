2b65515e1fd00f9f9e402035096239b9ea3586a0
iTzVoko <vokoads@gmail.com>
2023-07-20 11:21:14 +0300

    Add `6.cpp` solution (38-46)

---
+++ assignments/38-46/6.cpp
@@ -0,0 +1,24 @@
+/*
+ * بسم الله الرحمن الرحيم
+ *
+ * Solved by: @iTzVoko
+ */
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int vals[] = {10, 20, 30};
+
+	// Write Your Code Here
+	vals[0] *= 10;
+	vals[1] *= 10;
+	vals[2] *= 10;
+
+	cout << vals[0] << "\n"; // 100
+	cout << vals[1] << "\n"; // 200
+	cout << vals[2] << "\n"; // 300
+
+	return 0;
+}
