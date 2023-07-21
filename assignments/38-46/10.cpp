95f62b69caca54f196cb27eeafb56ca3209556f5
iTzVoko <vokoads@gmail.com>
2023-07-22 00:14:36 +0300

    Add `10.cpp` solution (38-46)

---
+++ assignments/38-46/10.cpp
@@ -0,0 +1,37 @@
+/*
+ * بسم الله الرحمن الرحيم
+ *
+ * Solved by: @iTzVoko
+ */
+#include <array>
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	array<int, 6> nums = {10, 20, 30, 40, 20, 50};
+
+	// Method 1
+	// "First: 10"
+	// "Last: 50"
+	cout << "First: " << nums[0] << '\n';
+	cout << "Last: " << nums[size(nums) - 1] << '\n';
+
+	// Method 2
+	// "First: 10"
+	// "Last: 50"
+	cout << "First: " << nums.at(0) << '\n';
+	cout << "Last: " << nums.at(nums.size() - 1) << '\n';
+
+	// Method 3
+	// "First: 10"
+	// "Last: 50"
+	cout << "First: " << nums.back() << '\n';
+	cout << "Last: " << nums.front() << '\n';
+
+	// There's also Method 4 to do that (Like the #9 assignment)
+	cout << "First: " << *begin(nums) << '\n';
+	cout << "Last: " << *(end(nums) - 1) << '\n';
+
+	return 0;
+}
