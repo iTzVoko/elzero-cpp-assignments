80ee09cf68f6eef9e972fdc5063832f675ba616c
iTzVoko <vokoads@gmail.com>
2023-07-24 00:02:05 +0300

    Add `11.cpp` solution (38-46)

---
+++ assignments/38-46/11.cpp
@@ -0,0 +1,23 @@
+#include <array>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+	// Do Not Edit The Next 3 Lines
+	int index = 1;
+	array<int, 3> oldNums = {10, 20, 30};
+	array<int, 3> newNums;
+
+	// Write Your Code Here
+	newNums = oldNums;
+	reverse(begin(newNums), end(oldNums));
+
+	// Do Not Edit The Next 3 Lines
+	cout << newNums[0] << "\n"; // 30
+	cout << newNums[1] << "\n"; // 20
+	cout << newNums[2] << "\n"; // 10
+
+	return 0;
+}
