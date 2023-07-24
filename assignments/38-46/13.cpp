66e8403ef6b935cc85e9a1c00a7eee2e2df7f999
iTzVoko <vokoads@gmail.com>
2023-07-24 19:11:03 +0300

    Add `13.cpp` solution (38-46) (finish)

---
+++ assignments/38-46/13.cpp
@@ -0,0 +1,28 @@
+/*
+ * بسم الله الرحمن الرحيم
+ *
+ * Solved by: @iTzVoko
+ */
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	string fName = "Elzero ";
+	string mName = "Web ";
+	string lName = "School";
+
+	// Output Needed
+	// Elzero Web School
+	cout << fName << mName << lName << '\n';
+
+	// Elzero Web School
+	cout << fName + mName + lName << '\n';
+
+	// Elzero Web School
+	cout << fName.append(mName).append(lName) << '\n';
+
+	return 0;
+}
