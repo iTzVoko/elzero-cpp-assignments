2f937d63779449215fa15673daacde459e03817c
iTzVoko <vokoads@gmail.com>
2023-07-24 23:28:36 +0300

    Add `2.cpp` solution (47-54)

---
+++ assignments/47-54/2.cpp
@@ -0,0 +1,35 @@
+/*
+ * بسم الله الرحمن الرحيم
+ *
+ * Solved by: @iTzVoko
+ */
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int index = 2;
+
+	for (;;) // <= Do Not Edit This Line
+	{
+		// If index variable is bigger than 8, then stop the loop (break)
+		if (index > 8)
+			break;
+
+		cout << index << "\n"; // From 2 To 10
+
+		index++;
+	}
+
+	// Output
+	// 2
+	// 3
+	// 4
+	// 5
+	// 6
+	// 7
+	// 8
+
+	return 0;
+}
