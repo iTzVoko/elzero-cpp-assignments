a0e5106ef887e7ee5cd8e6cea29c7ce9cfe4c9df
iTzVoko <vokoads@gmail.com>
2023-07-24 23:22:31 +0300

    Add `1.cpp` solution (47-54)

---
+++ assignments/47-54/1.cpp
@@ -0,0 +1,70 @@
+/*
+ * بسم الله الرحمن الرحيم
+ *
+ * Solved by: @iTzVoko
+ */
+#include <iostream>
+
+int main()
+{
+	// For Output
+	// 0
+	// 1
+	// 2
+	// 3
+	// 4
+	// 5
+	// 6
+	// 7
+	// 8
+	// 9
+	// 10
+	for (int i = 0; i <= 10; i++)
+	{
+		std::cout << i << '\n';
+	}
+
+	// While Output
+	// 0
+	// 1
+	// 2
+	// 3
+	// 4
+	// 5
+	// 6
+	// 7
+	// 8
+	// 9
+	// 10
+	int i = 0;
+
+	while (i <= 10)
+	{
+		std::cout << i << '\n';
+
+		i++;
+	}
+
+	// Do While Output
+	// 0
+	// 1
+	// 2
+	// 3
+	// 4
+	// 5
+	// 6
+	// 7
+	// 8
+	// 9
+	// 10
+	int x = 0;
+
+	do
+	{
+		std::cout << x << '\n';
+
+		x++;
+	} while (x <= 10);
+
+	return 0;
+}
