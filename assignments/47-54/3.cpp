0aa44fb467f053157d6bbdfe922b4f46b0498907
iTzVoko <vokoads@gmail.com>
2023-07-25 23:45:51 +0300

    Update `3.cpp` solution (47-54): remove unnecessary curly brackets

--- assignments/47-54/3.cpp
+++ assignments/47-54/3.cpp
@@ -23,42 +23,41 @@ int main()
 		Number Two: 2
 		Output: 3, 5, 7
 	*/
+
+	int rangeStart, rangeEnd;
+
+	std::cout << "Enter the start and the end of the range: ";
+	std::cin >> rangeStart >> rangeEnd;
+
+	// If the range start was bigger than the end, swap their values
+	if (rangeStart > rangeEnd)
 	{
-		int rangeStart, rangeEnd;
-
-		std::cout << "Enter the start and the end of the range: ";
-		std::cin >> rangeStart >> rangeEnd;
-
-		// If the range start was bigger than the end, swap their values
-		if (rangeStart > rangeEnd)
-		{
-			int temp = rangeStart;
-			rangeStart = rangeEnd;
-			rangeEnd = temp;
-		}
-
-		// increment it; so the start number will not be printed
-		rangeStart++;
-
-		for (int i = rangeStart; i < rangeEnd; i++)
-		{
-			// If number is even
-			if (i % 2 == 0)
-				continue;
-
-			// If it's the first number, print the number only without comma
-			if (i == rangeStart || i == rangeStart + 1)
-			{
-				std::cout << i;
-				continue; // Continue the loop without executing below line
-			}
-
-			// If It's not the first number, print comma then the number
-			std::cout << ", " << i;
-		}
-
-		std::cout << '\n';
+		int temp = rangeStart;
+		rangeStart = rangeEnd;
+		rangeEnd = temp;
 	}
+
+	// increment it; so the start number will not be printed
+	rangeStart++;
+
+	for (int i = rangeStart; i < rangeEnd; i++)
+	{
+		// If number is even
+		if (i % 2 == 0)
+			continue;
+
+		// If it's the first number, print the number only without comma
+		if (i == rangeStart || i == rangeStart + 1)
+		{
+			std::cout << i;
+			continue; // Continue the loop without executing below line
+		}
+
+		// If It's not the first number, print comma then the number
+		std::cout << ", " << i;
+	}
+
+	std::cout << '\n';
 
 	return 0;
 }
