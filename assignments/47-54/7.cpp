e80f32dc8b3663d0323b615a8c00822be616ad91
iTzVoko <vokoads@gmail.com>
2023-07-25 22:44:13 +0300

    Add `7.cpp` solution (47-54)

---
+++ assignments/47-54/7.cpp
@@ -0,0 +1,35 @@
+/*
+ * بسم الله الرحمن الرحيم
+ *
+ * Solved by: @iTzVoko
+ */
+#include <iostream>
+
+int main()
+{
+	// Output Needed
+	// 2
+	// 4
+	// 8
+	// 16
+	// 32
+	// 64
+	// 128
+	// For loop
+	for (int i = 2; i <= 128; i *= 2)
+	{
+		std::cout << i << '\n';
+	}
+
+	// While loop
+	int i = 2;
+
+	while (i <= 128)
+	{
+		std::cout << i << '\n';
+
+		i *= 2;
+	}
+
+	return 0;
+}
