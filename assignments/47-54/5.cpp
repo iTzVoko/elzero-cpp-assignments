718b297a3318cd7e5c3b0040a264865028a38bf9
iTzVoko <vokoads@gmail.com>
2023-07-25 22:30:57 +0300

    Add `5.cpp` solution (47-54)

---
+++ assignments/47-54/5.cpp
@@ -0,0 +1,38 @@
+/*
+ * بسم الله الرحمن الرحيم
+ *
+ * Solved by: @iTzVoko
+ */
+#include <iostream>
+
+int main()
+{
+	// Output Needed
+	// 0
+	// 3
+	// 6
+	// 9
+	// 12
+	// 15
+	// 18
+	// 21
+	// 24
+	// 27
+	// For loop
+	for (int i = 0; i <= 27; i += 3)
+	{
+		std::cout << i << '\n';
+	}
+
+	// While loop
+	int i = 0;
+
+	while (i <= 27)
+	{
+		std::cout << i << '\n';
+
+		i += 3;
+	}
+
+	return 0;
+}
