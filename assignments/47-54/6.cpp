6cfd2eb98423086555d5aa2491482ea581ba4406
iTzVoko <vokoads@gmail.com>
2023-07-25 22:38:54 +0300

    Add `6.cpp` solution (47-54)

---
+++ assignments/47-54/6.cpp
@@ -0,0 +1,32 @@
+/*
+ * بسم الله الرحمن الرحيم
+ *
+ * Solved by: @iTzVoko
+ */
+#include <iostream>
+
+int main()
+{
+	// Output Needed
+	// 10
+	// 100
+	// 10000
+	// 100000000
+	// For loop
+	for (int i = 10; i <= 100000000; i *= i)
+	{
+		std::cout << i << '\n';
+	}
+
+	// While loop
+	int i = 10;
+		
+	while (i <= 100000000)
+	{
+		std::cout << i << '\n';
+
+		i *= i;
+	}
+
+	return 0;
+}
