4a1719c5c62beb057413f148280a12df885810ee
iTzVoko <vokoads@gmail.com>
2023-07-25 22:24:00 +0300

    Update `4.cpp` solution (47-54): add while loop

---
+++ assignments/47-54/4.cpp
@@ -0,0 +1,40 @@
+/*
+ * بسم الله الرحمن الرحيم
+ *
+ * Solved by: @iTzVoko
+ */
+#include <iostream>
+
+int main()
+{
+	// Output Needed
+	// 0
+	// 2
+	// 4
+	// 6
+	// 8
+	// 14
+	// 16
+	// 18
+	// For loop
+	for (int i = 0; i <= 18; i += 2)
+	{
+		if (i == 10 || i == 12)
+			continue;
+
+		std::cout << i << '\n';
+	}
+
+	// While loop
+	int i = 0;
+
+	while (i <= 18)
+	{
+		if (i != 10 && i != 12)
+			std::cout << i << '\n';
+
+		i += 2;
+	}
+
+	return 0;
+}
