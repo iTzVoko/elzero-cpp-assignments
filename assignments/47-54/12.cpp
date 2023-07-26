29c0c469eec4b2f950bf2c489838aa8ee3e99994
iTzVoko <vokoads@gmail.com>
2023-07-26 12:50:47 +0300

    Add `12.cpp` solution (47-54)

---
+++ assignments/47-54/12.cpp
@@ -0,0 +1,26 @@
+/*
+ * بسم الله الرحمن الرحيم
+ *
+ * Solved by: @iTzVoko
+ */
+#include <iostream>
+
+int main()
+{
+	int nums[] = {10, 20, 14, 28, 40, 80, 15, 30};
+	int numsCount = std::size(nums);
+
+	// Output Needed
+	// 10
+	// 14
+	// 40
+	// 15
+
+	for (int i = 0; i < numsCount; i++)
+	{
+		if (nums[i] + nums[i] == nums[i + 1])
+			std::cout << nums[i] << '\n';
+	}
+
+	return 0;
+}
