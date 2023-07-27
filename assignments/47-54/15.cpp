f8927050a3488f066ec59cb4775bb90652921e7a
iTzVoko <vokoads@gmail.com>
2023-07-27 16:05:37 +0300

    Update `15.cpp` solution (47-54): add note and use `help_num` in `i`

--- assignments/47-54/15.cpp
+++ assignments/47-54/15.cpp
@@ -2,6 +2,10 @@
  * بسم الله الرحمن الرحيم
  *
  * Solved by: @iTzVoko
+ * Note: The size of element - 1 - i (current index) will always give
+ *		 you the index from the right (if the index is 0, it will give
+ *		 you the last element. if it's 1, it will give you the before the
+ *		 last element, and so on)
  */
 #include <iostream>
 
@@ -17,8 +21,9 @@ int main()
 	// "6 + 4 = 10"
 	// "10 + 2 = 12"
 
-	for (int i = 0; i <= help_num; i++)
+	for (int i = help_num - help_num; i <= help_num; i++)
 	{
+		// Read the note in the start of file
 		std::cout
 			<< nums[i] << " + " << nums[help_num - i]
 			<< " = " << nums[i] + nums[help_num - i] << '\n';
