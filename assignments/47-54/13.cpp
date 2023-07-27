b1cbc36060066073acbf8076ad5c6dcce829c8ab
iTzVoko <vokoads@gmail.com>
2023-07-27 10:32:36 +0300

    Add `13.cpp` solution (47-54)

---
+++ assignments/47-54/13.cpp
@@ -0,0 +1,43 @@
+/*
+ * بسم الله الرحمن الرحيم
+ *
+ * Solved by: @iTzVoko
+ */
+#include <iostream>
+
+int main()
+{
+	// Output Needed
+	// 30
+	// 27
+	// 24
+	// 21
+	// 18
+	// 15
+	// 12
+	// 9
+	// 6
+	// 3
+
+	// Without Even Numbers
+	// 27
+	// 21
+	// 15
+	// 9
+	// 3
+
+	for (int i = 30; i >= 3; i -= 3)
+	{
+		std::cout << i << '\n';
+	}
+
+	std::cout << "============\n";
+
+	for (int i = 30; i >= 3; i -= 3)
+	{
+		if (i % 2 != 0)
+			std::cout << i << '\n';
+	}
+
+	return 0;
+}
