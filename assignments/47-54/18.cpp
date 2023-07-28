b7e5156b07916c31f0c07fbbadc45911934c118c
iTzVoko <vokoads@gmail.com>
2023-07-28 23:52:37 +0300

    Add `18.cpp` solution (47-54)

---
+++ assignments/47-54/18.cpp
@@ -0,0 +1,30 @@
+/*
+ * بسم الله الرحمن الرحيم
+ *
+ * Solved by: @iTzVoko
+ */
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	for (int i = 0; i <= 1000; i += 100) // Do Not Edit This Line
+	{
+		// Edit What You Need And Add Your Code
+		if (i > 0 && i < 1000)
+			cout << i << "\n";
+	}
+
+	// Output Needed
+	// 100
+	// 200
+	// 300
+	// 400
+	// 600
+	// 700
+	// 800
+	// 900
+
+	return 0;
+}
