0936e879c5799e74a2106d56e9d44f45be85e808
iTzVoko <vokoads@gmail.com>
2023-07-29 02:10:32 +0300

    Add `1.cpp` solution (55-66)

---
+++ assignments/55-66/1.cpp
@@ -0,0 +1,23 @@
+/*
+ * بسم الله الرحمن الرحيم
+ *
+ * Solved by: @iTzVoko
+ * Note: Watch nested ternary operator video to understand the code:
+ * 		 https://youtu.be/8ptGpQ_2Np8
+ */
+#include <iostream>
+using namespace std;
+
+// Write Your Function Here
+int calcspecial(int first, int second)
+{
+	return (first == second ? first + second : (first > second ? first - second : second - first));
+}
+
+int main()
+{
+	cout << calcspecial(40, 40) << "\n";   // First = Second -> 40 + 40 = 80
+	cout << calcspecial(200, 50) << "\n";  // First > Second -> 200 - 50 = 150
+	cout << calcspecial(100, 300) << "\n"; // First < Second -> 300 - 100 = 200
+	return 0;
+}
