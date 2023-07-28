8f222ca0e5bd47469cd5bb09c9c22cbe68302184
iTzVoko <vokoads@gmail.com>
2023-07-28 23:58:26 +0300

    Add `20.cpp` solution (47-54)

---
+++ assignments/47-54/20.cpp
@@ -0,0 +1,29 @@
+/*
+ * بسم الله الرحمن الرحيم
+ *
+ * Solved by: @iTzVoko
+ */
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	string names[] = {"Osama", "Ahmed", "Mahmoud", "Hagar", "Eman", "Salwa"};
+	int namesCount = size(names);
+
+	// Output Needed
+	// "Osama"
+	// "Ahmed"
+	// "Hagar"
+	// "Salwa"
+
+	for (int i = 0; i < namesCount; i++)
+	{
+		if (names[i].size() == 5)
+			cout << names[i] << '\n';
+	}
+
+	return 0;
+}
